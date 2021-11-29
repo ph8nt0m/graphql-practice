70eb42cf8ab7adb27bc639ce1727b1b0cec89e01
ph8nt0m <phant0m@kakao.com>
2021-11-29 19:04:50 +0900

    UDPATE

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const { ApolloServer } = require("apollo-server");
 
 const typeDefs = `
+    enum PhotoCategory {
+      SELFIE
+      PORTRAIT
+      ACTION
+      LANDSCAPE
+      GRAPHIC
+    }
+
     type Photo {
         id: ID!
         url: String!
         name: String!
         description: String
+        category: PhotoCategory!
+        postedBy: User!
+    }
+    
+    input PostPhotoInput {
+      name: String!
+      category: PhotoCategory=PORTRAIT
+      description: String
+    }
+
+    type User {
+      githubLogin: ID!
+      name: String
+      avatar: String
+      postedPhotos: [Photo!]!
     }
 
     type Query {
@@ -14,12 +37,44 @@ const typeDefs = `
     }
 
     type Mutation {
-        postPhoto(name: String! description: String!): Photo!
+        postPhoto(input: PostPhotoInput!): Photo!
     }
 `;
 
 var _id = 0;
-var photos = [];
+var photos = [
+  {
+    id: '1',
+    name: 'Dropping The Cake',
+    description: '설명1',
+    category: 'ACTION',
+    githubUser: 'gPlake'
+  },
+  {
+    id: '2',
+    name: 'Dropping The Cake2',
+    description: '설명2',
+    category: 'SELFIE',
+    githubUser: 'mHattrup'
+  },
+  {
+    id: '3',
+    name: 'Dropping The Cake3',
+    description: '설명1',
+    category: 'LANDSCAPE',
+    githubUser: 'mHattrup'
+  },
+];
+var users = [
+  {
+    githubLogin: 'mHattrup',
+    name: 'Mike Hattrup'
+  },
+  {
+    githubLogin: 'gPlake',
+    name: 'Glen Plake'
+  }
+]
 
 const resolvers = {
   Query: {
@@ -31,7 +86,7 @@ const resolvers = {
     postPhoto(parent, args) {
       var newPhoto = {
         id: _id++,
-        ...args,
+        ...args.input,
       };
       photos.push(newPhoto);
 
@@ -40,7 +95,15 @@ const resolvers = {
   },
   Photo: {
     url: (parent) => `http://naver.com/img/${parent.id}.jpg`,
+    postedBy: parent => {
+      return users.find(u => u.githubLogin === parent.githubUser)
+    }
   },
+  User: {
+    postedPhotos: parent => {
+      return photos.filter(p => p.githubUser === parent.githubLogin)
+    }
+  }
 };
 
 const server = new ApolloServer({
