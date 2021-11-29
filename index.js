b376f0a85b6df81686a2b9b66a144f9593bbe1b6
ph8nt0m <phant0m@kakao.com>
2021-11-29 12:32:54 +0900

    UPDATE

---
+++ index.js
@@ -0,0 +1,53 @@
+const { ApolloServer } = require("apollo-server");
+
+const typeDefs = `
+    type Photo {
+        id: ID!
+        url: String!
+        name: String!
+        description: String
+    }
+
+    type Query {
+        totalPhotos: Int!
+        allPhotos: [Photo!]!
+    }
+
+    type Mutation {
+        postPhoto(name: String! description: String!): Photo!
+    }
+`;
+
+var _id = 0;
+var photos = [];
+
+const resolvers = {
+  Query: {
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos,
+  },
+
+  Mutation: {
+    postPhoto(parent, args) {
+      var newPhoto = {
+        id: _id++,
+        ...args,
+      };
+      photos.push(newPhoto);
+
+      return newPhoto;
+    },
+  },
+  Photo: {
+    url: (parent) => `http://naver.com/img/${parent.id}.jpg`,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server
+  .listen()
+  .then(({ url }) => console.log(`GraphQL Service running on ${url}`));
